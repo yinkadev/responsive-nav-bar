0f9c53bb5865a0865e1c59e6cbacb0a3624a6d55
yinkadev <109293802+yinkadev@users.noreply.github.com>
2022-10-11 12:59:55 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+$(function(){
+  $('.toggle').on('click', function(){
+ if($('.item').hasClass('active')){
+  $('.item').removeClass('active');
+ }
+ else{
+  $('.item').addClass('active');
+ }
+
+})
+
+})
